fc45494030833fc189696618235f2f8630eb0515
diana <diana@DESKTOP-AE9RS0A>
2022-03-07 00:31:47 +0300

    add ViewModelTask2

---
+++ TaskTest/TaskTest/ViewModel/ViewModelTask2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Data;
+using TaskTest.Model;
+
+namespace TaskTest.ViewModel
+{
+    public class ViewModelTask2 : INotifyPropertyChanged
+    {
+        IDialogService dialogService;
+        private RelayCommand openFileDialog;
+        public RelayCommand OpenFileDialog
+        {
+            get
+            {
+                return openFileDialog ??
+                    (openFileDialog = new RelayCommand(obj =>
+                    {
+                        if (dialogService.OpenFileDialog() == true)
+                        {
+                            
+                        }
+                    }));   
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
