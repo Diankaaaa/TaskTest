edac66db65208e0c6d49fc2f4b57ac14e656109c
diana <diana@DESKTOP-AE9RS0A>
2022-03-06 23:47:16 +0300

    add IDialogService and DefaultDialogService

---
+++ TaskTest/TaskTest/Model/IDialogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTest.Model
+{
+    public interface IDialogService
+    {
+        void ShowMessage(string message); //Показ сообщения
+        string FilePath { get; set; } //Путь к выбранному файлу
+        bool OpenFileDialog(); //Открытие файла
+        bool SaveFileDialog(); //Сохранение файла
+    }
+}
