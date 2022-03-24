aa992d40a3f6ff680fbf071934dc9ffd2f4c2b67
John Doe <johndoe@example.com>
2022-03-24 12:09:02 +0200

    24_03

--- TaskTest/TaskTest/MainWindow.xaml.cs
+++ TaskTest/TaskTest/MainWindow.xaml.cs
@@ -23,71 +23,11 @@ namespace TaskTest
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int task1, task2;
-        private string input2, outPut2, input3, outPut3;
         public MainWindow()
         {
-            input2 = "input2.txt";
-            outPut2 = "output2.txt";
             InitializeComponent();
             DataContext = new ViewModelTask();
         }
 
-
-        private async void Task1()
-        {
-            Factorial();
-            TASK1.Text = task1.ToString();
-        }
-
-        private async void Task2()
-        {
-
-        }
-
-
-        private async void ConvertTime()
-        {
-            DateTime.Now.ToString("HH:mm:ss");
-
-            //Чтение из файла
-            using (StreamReader reader = new StreamReader(input3))
-            {
-                string text = await reader.ReadToEndAsync();
-                MessageBox.Show(input3);
-            }
-
-            //Запись в файл
-            using (StreamWriter writer = new StreamWriter(outPut3, true, System.Text.Encoding.Default))
-            {
-                await writer.WriteLineAsync(task1.ToString());
-            }
-        }
-
-
-        private async void Factorial()
-        {
-            int F = 1;
-            for(int i = 1; i<=F; i++)
-            {
-                F = F * i;
-            }
-
-            task1 = F;
-
-            //Чтение из файла
-            using (StreamReader reader = new StreamReader(input2))
-            {
-                string text = await reader.ReadToEndAsync();
-                MessageBox.Show(input2);
-            }
-
-            //Запись в файл
-            using (StreamWriter writer = new StreamWriter(outPut2, true, System.Text.Encoding.Default))
-            {
-                await writer.WriteLineAsync(task1.ToString());
-            }
-
-        }
     }
 }
--- TaskTest/TaskTest/ViewModel/ViewModelTask.cs
+++ TaskTest/TaskTest/ViewModel/ViewModelTask.cs
@@ -15,79 +15,10 @@ namespace TaskTest.ViewModel
     {
         public ViewModelTask()
         {
-            Task2();
-            Task3();
+            
         }
 
         
-
-        Task task2 = new Task(() => {
-            Factorial num = new Factorial(5);
-            string s = "";
-            for (int i = 1; i <= int.Parse(num.ToString()); i++)
-            {
-                i = i * int.Parse(num.ToString());
-                s = i.ToString();
-            }
-        });
-            //task2.Start();
-            //task2.Id;
-            //task2.IsCompleted;
-            //task2.Status;
-            //task2.Wait();
-
-
-
-        private Task Task2()
-        {
-            return task2;
-        }
-
-
-
-
-
-        Task task3 = new Task(() => {
-            TimeTask time = new TimeTask();
-            DateTime dt = Convert.ToDateTime(time);
-            String militaryDate = dt.ToString("HH:mm:sstt");
-        });
-        //task3.Start();
-        //task3.Id;
-        //task3.IsCompleted;
-        //task3.Status;
-        //task3.Wait();
-
-        private Task Task3()
-        {
-            return task3;            
-        }
-
-        private RelayCommand factorialTask;
-        public RelayCommand FactorialTask
-        {
-            get
-            {
-                return factorialTask ??
-                    (factorialTask = new RelayCommand(obj =>
-                    {
-                        Task2();
-                    }));
-            }
-        }
-
-        private RelayCommand dateConvert;
-        public RelayCommand DateConvert
-        {
-            get
-            {
-                return dateConvert ??
-                    (dateConvert = new RelayCommand(obj =>
-                   {
-                       Task3();
-                   }));
-            }
-        }
 
 
         public event PropertyChangedEventHandler PropertyChanged;
