1030cad7de759e96cc1d7e73d6e2f397c0f8a297
John Doe <johndoe@example.com>
2022-04-06 12:13:40 +0200

    06_04

--- TaskTest/TaskTest/ViewModel/DateConvertTask.cs
+++ TaskTest/TaskTest/ViewModel/DateConvertTask.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using TaskTest.Model;
+using System.Data;
+using System.IO;
+using System.Threading;
 
 namespace TaskTest.ViewModel
 {
-    internal class DateConvertTask
+    public class DateConvertTask
     {
+        public string DateInput { get;set;}
+        public string DateOut { get; set; }
 
+        public DateConvertTask()
+        {
+            DateInput = Read();
+            DateOut = Convert(DateInput);
+        }
+
+        private string Convert(string dateTime)
+        {
+            DateTime dateTimes = DateTime.ParseExact(dateTime, "hh:mm:sstt", 
+        System.Globalization.CultureInfo.InvariantCulture); 
+            return dateTime;
+        }
+        private string Read()
+        {
+            Thread.Sleep(2000);
+            string text = "";
+            using (StreamReader reader = new StreamReader("input3.txt"))
+            {
+                text = reader.ReadToEnd();
+            }
+            return text;
+        }
     }
 }
+
--- TaskTest/TaskTest/ViewModel/MainViewModel.cs
+++ TaskTest/TaskTest/ViewModel/MainViewModel.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using TaskTest.Model;
+using System.Data;
+using System.IO;
+using System.Threading;
 
 namespace TaskTest.ViewModel
 {
     internal class MainViewModel
     {
         public FactorialTask Factorials { get; set; }
+        public DateConvertTask Dates { get;set;}
         public MainViewModel()
         {
             Factorials = new FactorialTask();
+            Dates = new DateConvertTask();
         }
         private RelayCommand click;
         public RelayCommand Click
@@ -22,6 +31,7 @@ namespace TaskTest.ViewModel
                     (click = new RelayCommand(obj =>
                     {
                         Factorials = new FactorialTask();
+                        Dates = new DateConvertTask();
                     }));
             }
         }
